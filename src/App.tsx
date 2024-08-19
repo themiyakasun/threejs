1178ea6d7688bf7c8281d53446ae3c7af198c967
themiyakasun <themiyakasun.tb@gmail.com>
2024-08-19 12:50:57 +0530

    Hemishphere

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const mountRef = useRef<HTMLDivElement>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  useEffect(() => {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
 
-export default App
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(w, h);
+
+    if (mountRef.current) {
+      mountRef.current.appendChild(renderer.domElement);
+    }
+
+    const fov = 90;
+    const aspect = w / h;
+    const near = 0.5;
+    const far = 20;
+
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
+
+    const scene = new THREE.Scene();
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.03;
+
+    const geo = new THREE.IcosahedronGeometry(1.0, 2);
+    const mat = new THREE.MeshStandardMaterial({
+      color: 0xfffff,
+      flatShading: true,
+    });
+
+    const mesh = new THREE.Mesh(geo, mat);
+    scene.add(mesh);
+
+    const wireMat = new THREE.MeshBasicMaterial({
+      color: 0xfffff,
+      wireframe: true,
+    });
+    const wireMesh = new THREE.Mesh(geo, wireMat);
+    wireMesh.scale.setScalar(1.001);
+    mesh.add(wireMesh);
+
+    const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+    scene.add(hemiLight);
+
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+      controls.update();
+    }
+
+    animate();
+  });
+
+  return <div ref={mountRef}></div>;
+};
+
+export default App;
